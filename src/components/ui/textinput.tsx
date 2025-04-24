8b8e74128b243f284260b70afd96049a18fc6135
Markus <markussmadsbakken@gmail.com>
2025-04-24 21:15:48 +0200

    Add cursor pointer to show password button

--- src/components/ui/textinput.tsx
+++ src/components/ui/textinput.tsx
@@ -52,7 +52,7 @@ export default function TextInput({
           <button
             type="button"
             onClick={() => setShowPassword(!showPassword)}
-            className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500"
+            className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"
           >
             {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
           </button>
