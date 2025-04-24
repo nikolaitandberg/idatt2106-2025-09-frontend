259dc32ff9ce33228b5525b382a497b83af45901
Markus <markussmadsbakken@gmail.com>
2025-04-24 19:20:16 +0200

    create auth provider, auth requests, and the Auth client

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,11 +1,16 @@
-"use client"
+"use client";
 
+import { AuthClient } from "@/util/authClient";
+import { AuthProvider } from "@/util/authProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
+const authClient = new AuthClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider client={authClient}>{children}</AuthProvider>
+    </QueryClientProvider>
   );
 }
---
+++ src/util/authProvider.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { Token } from "@/types";
+import { createContext, useContext, useMemo } from "react";
+
+export interface AuthProviderClient {
+  login: (
+    username: string,
+    password: string,
+  ) => Promise<AuthProviderLoginSuccessResponse | AuthProviderLoginErrorResponse>;
+  register: (
+    username: string,
+    password: string,
+  ) => Promise<AuthProviderLoginSuccessResponse | AuthProviderLoginErrorResponse>;
+  logout: () => void;
+  isAuthenticated: boolean;
+  token: Token | null;
+  rawToken: string | null;
+}
+
+interface AuthContext {
+  client: AuthProviderClient;
+}
+
+export type AuthProviderLoginSuccessResponse = {
+  success: true;
+  token: string;
+};
+
+export type AuthProviderLoginErrorResponse = {
+  success: false;
+  message: string;
+};
+
+const AuthContext = createContext<AuthContext>({} as AuthContext);
+
+export function AuthProvider({ client, children }: { client: AuthProviderClient; children: React.ReactNode }) {
+  const contextValue = useMemo(() => ({ client }), [client.token, client.rawToken, client.isAuthenticated]);
+
+  return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
+}
+
+/**
+ * Hook that returns the auth client
+ * @returns
+ */
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("AuthProvider is not used in the component tree");
+  }
+  return context.client;
+}
+
+/**
+ * Gets the token and authentication status
+ * @returns
+ */
+export function useToken(): Readonly<{ token: Token | null; isAuthenticated: boolean }> {
+  const { token, isAuthenticated } = useAuth();
+  return {
+    token,
+    isAuthenticated,
+  };
+}
+
+/**
+ * Hook that returns the login function
+ */
+export function useLogin() {
+  const { login } = useAuth();
+  return login;
+}
+
+/**
+ * Hook that returns the register function
+ */
+export function useRegister() {
+  const { register } = useAuth();
+  return register;
+}
+
+/**
+ * Hook that returns the logout function
+ */
+export function useLogout() {
+  const { logout } = useAuth();
+  return logout;
+}
