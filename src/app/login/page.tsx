a6c15a7ba869b03b325f62891ad21371e61a7133
Håkon Duås Bjerkestrand <haakodbj@stud.ntnu.no>
2025-04-24 15:28:31 +0200

    add LoginPage with TextInput and Button components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,33 @@
-export default function loginPage() {
+"use client";
+
+import TextInput from "@/components/ui/textinput";
+import { Button } from "@/components/ui/button";
+
+export default function LoginPage() {
   return (
-    <div>
-      <h1>Inlogging</h1>
+    <div className="min-h-screen flex items-center justify-center bg-[FFF9F8] px-4">
+      <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8 space-y-6">
+        <h1 className="text-2xl font-bold text-center">Logg inn</h1>
+        <div>
+          <TextInput
+            label="E-post"
+            name="email"
+            type="email"
+            placeholder="din@epost.no"
+            validate={(value) => value.includes("@")}
+          />
+
+          <TextInput
+            label="Passord"
+            name="password"
+            type="password"
+            placeholder="••••••••"
+            validate={(value) => value.length >= 6}
+          />
+
+          <Button className="size-full">Logg inn</Button>
+        </div>
+      </div>
     </div>
   );
 }
