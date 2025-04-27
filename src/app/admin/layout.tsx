8b18bb3a1ce846492594390f15b88d57a92f88f3
Markus <markussmadsbakken@gmail.com>
2025-04-27 13:54:52 +0200

    Fix min page size

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,8 +2,8 @@ import { AdminMenu } from "@/components/adminMenu";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-full">
-      <div className="min-w-fit min-h-screen">
+    <div className="flex h-full flex-1">
+      <div className="min-w-fit">
         <AdminMenu />
       </div>
       <div className="flex-1/2 p-6">{children}</div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
         <Providers>
           <Topbar />
           {children}
