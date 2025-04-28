0ee053ada68b4825fbe13a57d6fd6637d9bd78f0
Håkon Duås Bjerkestrand <haakodbj@stud.ntnu.no>
2025-04-28 12:43:04 +0200

    add api functions for fetching and updating household and users

--- src/actions/household.ts
+++ src/actions/household.ts
@@ -1,24 +1,52 @@
-import { HouseholdResponse } from "@/types/household";
+import { ApiError } from "@/types/apiResponses";
 import { API_BASE_URL } from "@/types/constants";
+import { HouseholdResponse, UserResponse } from "@/types/household";
+import Fetch, { FetchFunction, useFetch } from "@/util/fetch";
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 
-export const getHousehold = async (householdId: number): Promise<HouseholdResponse> => {
-  const res = await fetch(`${API_BASE_URL}/households/${householdId}`);
-  
-  if (!res.ok) {
-    throw new Error("Failed to fetch household");
+export const getHousehold = async (id: number, fetcher: FetchFunction = Fetch): Promise<HouseholdResponse | null> => {
+  try {
+    const res = await fetcher<HouseholdResponse>(`${API_BASE_URL}/households/${id}`);
+    return res ?? null;
+  } catch (error) {
+    if (error instanceof ApiError && error.code === 404) {
+      return null;
+    }
+    throw error;
   }
-
-  const data: HouseholdResponse = await res.json();
-  return data;
 };
 
-export const getAllHouseholds = async (): Promise<HouseholdResponse[]> => {
-  const res = await fetch(`${API_BASE_URL}/households`);
+export const getHouseholdUsers = async (id: number, fetcher: FetchFunction = Fetch): Promise<UserResponse[]> => {
+  const res = await fetcher<UserResponse[]>(`${API_BASE_URL}/households/${id}/users`);
+  return res ?? [];
+};
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch households");
-  }
+export const addUserToHousehold = async (data: { username: string; householdId: number }, fetcher: FetchFunction = Fetch) => {
+  await fetcher(`${API_BASE_URL}/households/add-user`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  });
+};
 
-  const data: HouseholdResponse[] = await res.json();
-  return data;
+export const useHousehold = (id: number, options?: UseQueryOptions<HouseholdResponse | null, Error>) => {
+  const fetcher = useFetch();
+
+  return useQuery<HouseholdResponse | null, Error>({
+    queryKey: ["household", id],
+    queryFn: () => getHousehold(id, fetcher),
+    enabled: options?.enabled ?? true,
+    ...options,
+  });
+};
+
+export const useHouseholdUsers = (id: number, options?: UseQueryOptions<UserResponse[], Error>) => {
+  const fetcher = useFetch();
+
+  return useQuery<UserResponse[], Error>({
+    queryKey: ["householdUsers", id],
+    queryFn: () => getHouseholdUsers(id, fetcher),
+    enabled: options?.enabled ?? true,
+    ...options,
+  });
 };
