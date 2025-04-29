8cef12d67608027ad7cd6bcb79dd5d0f7c296bf0
Markus <markussmadsbakken@gmail.com>
2025-04-29 20:32:48 +0200

    Add position and time selectors to available form elements

--- src/util/formContext.ts
+++ src/util/formContext.ts
@@ -2,6 +2,8 @@ import IconPicker from "@/components/ui/form/iconPicker";
 import NumberInput from "@/components/ui/form/numberInput";
 import SubmitButton from "@/components/ui/form/submitButton";
 import TextInput from "@/components/ui/form/textInput";
+import PositionSelector from "@/components/ui/positionSelector";
+import TimeSelector from "@/components/ui/timeSelector";
 import { createFormHookContexts, createFormHook } from "@tanstack/react-form";
 
 export const { fieldContext, formContext, useFieldContext, useFormContext } = createFormHookContexts();
@@ -13,6 +15,8 @@ const { useAppForm } = createFormHook({
     TextInput,
     NumberInput,
     IconPicker,
+    TimeSelector,
+    PositionSelector,
   },
   formComponents: {
     SubmitButton,
