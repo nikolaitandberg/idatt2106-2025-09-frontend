c9838ce63c9f8104764b387650dff8b29c116f1d
Markus <113692526+MarkussMadsbakken@users.noreply.github.com>
2025-05-06 17:41:08 +0200

    make ws use wss if using https

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,4 +1,4 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const USE_HTTPS = process.env.NEXT_PUBLIC_USE_HTTPS === "true";
 export const API_BASE_URL = `${USE_HTTPS ? "https" : "http"}://${API_URL ?? "localhost:8080"}/api`;
-export const WS_BASE_URL = `ws://${API_URL}/ws`;
+export const WS_BASE_URL = `${USE_HTTPS ? "wss" : "ws":}//${API_URL}/ws`;
