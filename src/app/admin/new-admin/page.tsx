e0a00b4f45040dc85c926822bc9058e0419f7203
nikolaitandberg <nikolai.tandbe@gmail.com>
2025-05-06 14:38:13 +0200

    add tables to admin-admin page

--- src/app/admin/new-admin/page.tsx
+++ src/app/admin/new-admin/page.tsx
@@ -1,8 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
 export default function AdminNews() {
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-4">Admin Management</h1>
-      {/* News management content */}
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-3xl font-bold mb-4">Administratorer</h1>
+        <Button variant="default">Inviter ny administrator</Button>
+      </div>
+
+      <h2>Aktive administratorer</h2>
+      <Table>
+        <TableCaption>Tabell over aktive administratorer</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-1/3">brukernavn</TableHead>
+            <TableHead>epost</TableHead>
+            <TableHead className="text-right">dato</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {/*<TableRow>*/}
+          {/*  <TableCell className="font-medium">INV001</TableCell>*/}
+          {/*  <TableCell>Paid</TableCell>*/}
+          {/*  <TableCell>Credit Card</TableCell>*/}
+          {/*  <TableCell className="text-right">$250.00</TableCell>*/}
+          {/*</TableRow>*/}
+        </TableBody>
+      </Table>
+
+      <h2>Invitasjoner</h2>
+      <Table>
+        <TableCaption>Tabell over utestående administrator-invitasjoner </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-1/3">brukernavn</TableHead>
+            <TableHead>epost</TableHead>
+            <TableHead className="text-right">epost</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {/*<TableRow>*/}
+          {/*  <TableCell className="font-medium">INV001</TableCell>*/}
+          {/*  <TableCell>Paid</TableCell>*/}
+          {/*  <TableCell>Credit Card</TableCell>*/}
+          {/*  <TableCell className="text-right">$250.00</TableCell>*/}
+          {/*</TableRow>*/}
+        </TableBody>
+      </Table>
+
     </div>
   );
 }
