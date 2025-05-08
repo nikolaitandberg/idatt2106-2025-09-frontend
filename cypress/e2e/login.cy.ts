c1f8f040294070ae8f34b6a671dcdfc6b2cf0044
Jacob Lein <jacoble@stud.ntnu.no>
2025-05-08 14:13:06 +0200

    Refactor login test to remove unnecessary profile checks and ensure login request is intercepted

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,12 +1,11 @@
 describe("login", () => {
   it("Should log in to olaNordmann and then log out", () => {
+    cy.intercept("POST", "/api/auth/callback/credentials").as("loginRequest");
     cy.visit("/login");
     cy.get("#username").should("be.visible").type("olaNordmann");
     cy.get("#password").type("Password12345");
 
     cy.get('[data-testid="submit-login"]').click();
-    cy.get('[data-testid="page-title"]', { timeout: 5000 }).should("contain.text", "Krisefikser");
-    cy.visit("/profile");
-    cy.get('[data-testid="profile-username"]').should("contain.text", "olaNordmann");
+    cy.wait("@loginRequest").its("response.statusCode").should("eq", 200);
   });
 });
