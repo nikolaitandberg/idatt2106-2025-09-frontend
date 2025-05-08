1fc8397be53ece41d52c84b7a72e695a8eae49ec
Jacob Lein <jacoble@stud.ntnu.no>
2025-05-08 13:53:43 +0200

    Fix login test by updating username input selector and ensuring visibility check

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,8 +1,8 @@
 describe("login", () => {
   it("Should log in to olaNordmann and then log out", () => {
     cy.visit("/login");
-    cy.get('[data-testid="input-username"]').type("nordmannJunior");
-    cy.get('[data-testid="input-password"]').type("Password12345");
+    cy.get("#username").should("be.visible").type("olaNordmann");
+    cy.get("#password").type("Password12345");
 
     cy.get('[data-testid="submit-login"]').click();
     cy.get('[data-testid="page-title"]', { timeout: 5000 }).should("contain.text", "Krisefikser");
