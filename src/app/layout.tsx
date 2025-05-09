5799bfdacea9356306864d4bb3cbc40f4b074281
Markus <markussmadsbakken@gmail.com>
2025-05-09 12:31:52 +0200

    Fix footer placement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,12 +23,14 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           <Topbar />
           {children}
           <Toaster />
-          <footer className="text-sm w-full flex justify-center items-center bg-gray-100 text-gray-700 p-4 gap-10">
-            <span>© {new Date().getFullYear()} Krisefikser™.</span>
-            <Link href="/privacy-policy" className="text-blue-700 hover:underline">
-              Personvernerklæring
-            </Link>
-          </footer>
+          <div className="flex-1 relative w-full">
+            <footer className="absolute bottom-0 justify-center flex items-center bg-gray-100 text-gray-700 p-4 gap-10 w-full">
+              <span>© {new Date().getFullYear()} Krisefikser™.</span>
+              <Link href="/privacy-policy" className="text-blue-700 hover:underline">
+                Personvernerklæring
+              </Link>
+            </footer>
+          </div>
         </Providers>
       </body>
     </html>
