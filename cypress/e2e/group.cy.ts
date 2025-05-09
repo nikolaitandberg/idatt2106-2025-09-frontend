563fbd081b2516a224e73c011a63f454eaa2aa68
Jacob Lein <jacoble@stud.ntnu.no>
2025-05-09 15:55:59 +0200

    Enhance group creation test to include invitation functionality

--- cypress/e2e/group.cy.ts
+++ cypress/e2e/group.cy.ts
@@ -18,25 +18,29 @@ describe("Group", () => {
     cy.get('a[href="/group"]', { timeout: 2000 }).click();
     cy.get("h1.text-xl.font-semibold", { timeout: 5000 }).should("be.visible").and("contain.text", "Beredskapsgruppe");
   });
-  it("Should be able to create group", () => {
+  it("Should be able to create group and invite", () => {
     cy.intercept("POST", "/api/emergency-groups").as("createGroup");
     cy.intercept("GET", "/group*").as("loadGroupPage");
 
     cy.visit("/group");
     cy.get("#name", { timeout: 3000 }).should("be.visible").clear().type("nordmanns gruppe");
-    cy.get("#description", { timeout: 3000 })
-      .should("be.visible")
-      .clear()
-      .type("Dette er en gruppe for alle i nordmann utvidet familie");
+    cy.get("#description").should("be.visible").clear().type("Dette er en gruppe for alle i nordmann utvidet familie");
 
-    cy.wait(5000);
     cy.contains("button", "Lag gruppe").should("be.enabled").click();
+
     cy.wait("@createGroup").its("response.statusCode").should("eq", 201);
     cy.wait("@loadGroupPage").its("response.statusCode").should("eq", 200);
 
-    cy.wait(10000);
-    cy.get("h1.text-2xl.font-semibold", { timeout: 5000 })
-      .should("be.visible")
-      .and("contain.text", "Dine beredskapsgrupper");
+    cy.contains("h3", "nordmanns gruppe").should("be.visible");
+
+    cy.get('a[href="/group/1"] > div.rounded-lg').should("be.visible").click();
+
+    cy.contains("h1", "nordmanns gruppe", { timeout: 3000 }).should("be.visible");
+
+    cy.contains("button", "Inviter en husholdning").should("be.visible").click();
+
+    cy.get("#householdId").should("be.visible").clear().type("2");
+    cy.contains("button", "Send invitasjon").should("be.visible").click();
+    cy.contains("p", "Husholdning 2 ble invitert til gruppen.").should("be.visible");
   });
 });
