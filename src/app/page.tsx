aaae6476047342aca5bdc1f1e0ce6714b262d199
Håkon Duås Bjerkestrand <haakodbj@stud.ntnu.no>
2025-05-09 15:40:57 +0200

    make image fit to content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function HomePage() {
       </section>
 
       <aside className="lg:w-1/2 w-full p-10">
-        <a href="/map" className="block relative h-[600px] rounded-xl shadow-sm overflow-hidden group">
+        <a href="/map" className="block relative h-full min-h-[400px] rounded-xl shadow-sm overflow-hidden group">
           <Image src="/kart-hjem.png" alt="Kart" fill className="object-cover" priority />
           <div className="absolute inset-0 bg-black/0 group-hover:bg-black/60 transition-colors duration-300 flex items-center justify-center">
             <span className="text-white text-xl font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
